66bb640e2287630b7068bef4e10a6cd5e1922b64
Simon Huesken <simon.huesken@gmail.com>
2020-04-02 15:57:36 +0200

    make standardjs

--- pong/babel.config.js
+++ pong/babel.config.js
@@ -1,8 +1,8 @@
-module.exports = function(api) {
-    api.cache(true);
+module.exports = function (api) {
+  api.cache(true)
 
-    const presets = ['@babel/preset-env'];
-    const plugins = [];
+  const presets = ['@babel/preset-env']
+  const plugins = []
 
-    return { presets, plugins };
-};
+  return { presets, plugins }
+}
--- pong/dist-server/client/clientEntryPoint.js
+++ pong/dist-server/client/clientEntryPoint.js
@@ -1,16 +1,15 @@
-"use strict";
+'use strict'
 
-var _queryString = _interopRequireDefault(require("query-string"));
+var _queryString = _interopRequireDefault(require('query-string'))
 
-var _Game = _interopRequireDefault(require("../common/Game"));
+var _Game = _interopRequireDefault(require('../common/Game'))
 
-var _lanceGg = require("lance-gg");
+var _lanceGg = require('lance-gg')
 
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+function _interopRequireDefault (obj) { return obj && obj.__esModule ? obj : { default: obj } }
 
-var qsOptions = _queryString.default.parse(location.search); // default options, overwritten by query-string options
+var qsOptions = _queryString.default.parse(location.search) // default options, overwritten by query-string options
 // is sent to both game engine and client engine
-
 
 var defaults = {
   traceLevel: _lanceGg.Lib.Trace.TRACE_NONE,
@@ -21,12 +20,12 @@ var defaults = {
     remoteObjBending: 0.8,
     bendingIncrements: 6
   }
-};
-var options = Object.assign(defaults, qsOptions); // create a client engine and a game engine
+}
+var options = Object.assign(defaults, qsOptions) // create a client engine and a game engine
 
-var gameEngine = new _Game.default(options);
-var clientEngine = new _lanceGg.ClientEngine(gameEngine, options, _lanceGg.Renderer);
+var gameEngine = new _Game.default(options)
+var clientEngine = new _lanceGg.ClientEngine(gameEngine, options, _lanceGg.Renderer)
 document.addEventListener('DOMContentLoaded', function (e) {
-  clientEngine.start();
-});
-//# sourceMappingURL=clientEntryPoint.js.map
+  clientEngine.start()
+})
+// # sourceMappingURL=clientEntryPoint.js.map
